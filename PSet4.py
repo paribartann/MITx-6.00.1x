7555f87059ceced8b52dd8540a139e6129a3035c
paribartann <paribartandhakal@gmail.com>
2018-07-06 20:15:58 -0500

    Update PSet4.py

--- PSet4.py
+++ PSet4.py
@@ -118,32 +118,6 @@ def playGame(wordList):
  #Problem 7 (18.00/20.00)
 
  def playGame(wordList):
-    """
-    Allow the user to play an arbitrary number of hands.
- 
-    1) Asks the user to input 'n' or 'r' or 'e'.
-        * If the user inputs 'e', immediately exit the game.
-        * If the user inputs anything that's not 'n', 'r', or 'e', keep asking them again.
-
-    2) Asks the user to input a 'u' or a 'c'.
-        * If the user inputs anything that's not 'c' or 'u', keep asking them again.
-
-    3) Switch functionality based on the above choices:
-        * If the user inputted 'n', play a new (random) hand.
-        * Else, if the user inputted 'r', play the last hand again.
-          But if no hand was played, output "You have not played a hand yet. 
-          Please play a new hand first!"
-        
-        * If the user inputted 'u', let the user play the game
-          with the selected hand, using playHand.
-        * If the user inputted 'c', let the computer play the 
-          game with the selected hand, using compPlayHand.
-
-    4) After the computer or user has played the hand, repeat from step 1
-
-    wordList: list (string)
-    """
-    # TO DO...
     gameCount=0
     while True:
     
@@ -182,14 +156,3 @@ def playGame(wordList):
                 #else:
                     #break
                 print()
-
-
-
-
-
-
-
-
-
-
-
